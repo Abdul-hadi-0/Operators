32cef005c17417fec9d86ee4e9c5a779c4d44cee
ABDUL.HADI <abdul.hadi034178@gmail.com>
2024-05-14 17:10:31 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,109 @@
+import chalk from 'chalk';
+console.log("Arthmetic Operators");
+
+
+var x: number = 10;
+var y: number = 5;
+
+var sum : number = x+y
+console.log(sum);
+var sum : number = x-y
+console.log(sum);
+var sum : number = x*y
+console.log(sum);
+var sum : number = x/y
+console.log(sum);
+var sum : number = x%y
+console.log(sum);
+
+
+
+//  console.log("Assignment Operators")
+
+// var x: number = 10;
+// var y: number = 5;
+
+// x += y
+// y += x
+// console.log(y);
+
+
+// console.log("String Operators");
+
+// var msg:string = "hello" + "world";
+// console.log(msg);
+
+// console.log("Logical operators");
+
+// var isBookAvailable: boolean = true
+// var userBorrowedBooks: number = 1;
+// const maxAllowBooks: number = 5;
+
+// if(isBookAvailable && userBorrowedBooks < maxAllowBooks){
+//    console.log("Book available");
+// } else {
+//     console.log("Book not available");
+// }
+
+//  console.log("Logical operators");
+
+//  var hasCouponCode: boolean = true;
+//  var orderAmount:number = 1000;
+//  var isMember: boolean = false;
+
+//  if (hasCouponCode || (orderAmount > 1000 && isMember)) {
+//     console.log (chalk.blue("Apko Discount mil gya hai")); }
+// else {
+//     console.log (chalk.red("Apko Discount nahi mil skta hai")); }
+
+// console.log("CONDITIONAL TERNARY OPERATORS");
+
+// condition or conditions ? expression 1 : expression 2
+
+// var userName = "hadi";
+// var passCode = "10";
+
+// userName == "hadi" && passCode < 100
+// ? console.log(chalk.greenBright("logged in succesfully"))
+// : console.log(chalk.red("invalid username or password"))
+
+
+// var isHungry = true;
+// var snack = isHungry ? "apple" : "water";
+// console.log(chalk.bgBlueBright(`you should have some ${snack}.`));
+
+// console.log(chalk.yellow("UNARY OPERATORS (INCREMENT & DECREMENT)"));
+
+// var x: number =5 ;
+
+//post increment 
+
+// var postIncrement: number = x++;
+
+// console.log("post increment value :" , postIncrement);
+// console.log("value after post increment :" , x);
+
+//pre increment
+
+// var preIncrement: number = ++x;
+
+// console.log("pre increment value :" , preIncrement);
+
+// console.log("value after pre increment :" , x);
+
+// var a: number =5 ;
+
+//post decrement 
+
+// var postDecrement: number = a--;
+
+// console.log("post decrement value :" , postDecrement);
+// console.log("value after post decrement :" , a);
+
+//pre decrement
+
+// var preDecrement: number= --a;
+
+// console.log("pre decrement value :" , preDecrement);
+
+// console.log("value after pre decrement :" , a);
